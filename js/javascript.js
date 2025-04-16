1a5be7a194ef29bc19f8252de2db7d99aa2c58a2
Paulo02-pixel <u20191e562@upc.edu.pe>
2025-04-15 20:37:10 -0500

    Landing completo

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("formContacto");
+
+    if (form) {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            const nombre = document.getElementById("nombre").value;
+            const correo = document.getElementById("correo").value;
+            const numero = document.getElementById("numero").value;
+
+            alert(`Gracias por contactarnos, ${nombre}!\nTe responderemos al correo: ${correo}.`);
+        });
+    }
+});
 
